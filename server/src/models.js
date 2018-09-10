9dfc675deb4452a6c54cb31d572b117bd935a2e5
Marshall Tuinier <mtuinier@gmail.com>
2018-09-10 11:14:40 -0700

    Create Mongo Schema

---
+++ server/src/models.js
@@ -0,0 +1,57 @@
+// Create a Mongo Schema to help visualize the db structure
+
+const mongoose = require("mongoose");
+const moment = require("moment");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+// The buildModel function abstracts a bit of boilerplate, and inserts timestamps into every model
+
+const buildModel = (name, schema) => {
+  return mongoose.model(name, new Schema(schema, { timestamps: true }));
+};
+
+const Folder = buildModel("Folder", {
+  name: String,
+  description: String,
+  shareWith: [
+    {
+      kind: String,
+      item: { type: ObjectId, refPath: "shareWith.kind" }
+    }
+  ],
+  parent: { type: ObjectId, ref: "folder" }
+});
+
+module.exports.Folder = Folder;
+
+module.exports.Team = Folder.discriminator(
+  "Team",
+  new Schema({}, { timestamps: true })
+);
+
+// Below we build the User model for the db
+
+module.exports.User = buildModel("User", {
+  name: {
+    type: String,
+    default: ""
+  },
+  firstname: String,
+  lastname: String,
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String
+  },
+  jobTitle: {
+    type: String,
+    default: ""
+  },
+  avatarColor: String,
+  team: { type: ObjectId, ref: "Team" },
+  role: String,
+  status: String
+});
